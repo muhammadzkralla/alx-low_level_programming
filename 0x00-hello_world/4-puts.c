3e7612a7a2fbfcb5592de2059e75359a72948cf0
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-02-16 11:37:38 -0800

    Puts Edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

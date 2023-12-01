fc9ebd505e8cbae442dbd9ec3393628f778de12c
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-12-01 21:55:52 +0200

    0-memset optimization

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte.
- * @s: pointer to put the constant
- * @b: constant
- * @n: max bytes to use
+ * *_memset - fills memory with a certain char.
+ * @s: pointer to the string to fill
+ * @b: char
+ * @n: number of bytes
  * Return: s
  */
 
@@ -12,7 +12,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; n > 0; i++, n--)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
